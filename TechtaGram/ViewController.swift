70342059cf1f9ab0ef6bacbb09fa759947cc4885
hana <hana.ballet0101@gmail.com>
2016-02-03 21:58:13 +0900

    first commit

--- TechtaGram/ViewController.swift
+++ TechtaGram/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet var cameraImageView :UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,40 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func album() {
+        
+    }
+    
+    @IBAction func post() {
+        
+    }
+    
+    @IBAction func takephoto() {
+        //
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            //
+            let picker = UIImagePickerController()
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.delegate = self
+            
+            //
+            picker.allowsEditing = true
+            
+            presentViewController(picker, animated: true, completion: nil)
+        } else {
+            //
+            print("error")
+        }
+    }
+    
+    @IBAction func save(){
+        
+    }
+    
+    @IBAction func colorfilter(){
+        
     }
 
 
